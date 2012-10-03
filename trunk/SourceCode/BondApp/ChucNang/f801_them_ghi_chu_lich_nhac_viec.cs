0f1783c4b5f2b57be89d1ae3436a34dcfe1c5aa2
linhdh@bkindex.com <linhdh@bkindex.com@10aa7a8a-6401-9ec4-2594-d953f148eb29>
2012-10-03 10:30:44 +0000

    thêm form: f801 - form thêm ghi chú cho lịch nhắc việc

---
+++ trunk/SourceCode/BondApp/ChucNang/f801_them_ghi_chu_lich_nhac_viec.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BondApp.ChucNang
+{
+    public partial class f801_them_ghi_chu_lich_nhac_viec : Form
+    {
+        public f801_them_ghi_chu_lich_nhac_viec()
+        {
+            InitializeComponent();
+        }
+    }
+}
