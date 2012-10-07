4f8e30d35d89e2a6839536a689d5f10109ba3d80
linhdh@bkindex.com <linhdh@bkindex.com@10aa7a8a-6401-9ec4-2594-d953f148eb29>
2012-10-07 09:04:35 +0000

    Add thêm form quản lý tham số nhắc việc

---
+++ trunk/SourceCode/BondApp/ChucNang/f104_quan_ly_tham_so_nhac_viec.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BondApp.ChucNang
+{
+    public partial class f104_quan_ly_tham_so_nhac_viec : Form
+    {
+        public f104_quan_ly_tham_so_nhac_viec()
+        {
+            InitializeComponent();
+        }
+    }
+}
