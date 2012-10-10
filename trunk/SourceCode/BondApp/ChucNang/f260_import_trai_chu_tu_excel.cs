6964f476a8c1e8557d805d77cc95c4ab3fff85ff
linhdh@bkindex.com <linhdh@bkindex.com@10aa7a8a-6401-9ec4-2594-d953f148eb29>
2012-10-10 05:06:36 +0000

    thêm form import excel

---
+++ trunk/SourceCode/BondApp/ChucNang/f260_import_trai_chu_tu_excel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BondApp.ChucNang
+{
+    public partial class f260_import_trai_chu_tu_excel : Form
+    {
+        public f260_import_trai_chu_tu_excel()
+        {
+            InitializeComponent();
+        }
+    }
+}
